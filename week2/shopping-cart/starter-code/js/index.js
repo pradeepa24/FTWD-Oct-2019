54a9407dcab0e18d271f72f920b578be2e029fea
nickborbe <nickborbe@gmail.com>
2019-10-08 10:37:44 -0400

    move line function

--- week2/shopping-cart/starter-code/js/index.js
+++ week2/shopping-cart/starter-code/js/index.js
@@ -1,3 +1,7 @@
+
+
+
+
 var calcButton = document.getElementById('calc');
 
 // iteration 1 version
@@ -23,6 +27,9 @@ calcButton.onclick = function(){
 
   let rows = document.getElementsByClassName('product');
 
+  let theTotal = 0;
+
+
   console.log(rows);
 
  for(let i = 0; i < rows.length; i++){
@@ -35,28 +42,106 @@ calcButton.onclick = function(){
 
   let quantity = Number(rows[i].querySelector('.qty > label > input').value);
 
+  let productTotal = Number(quantity * unitPrice)
+
   
   let sutToteSpan = rows[i].querySelector('.subtot > span');
 
-  sutToteSpan.innerText = quantity * unitPrice;
+  sutToteSpan.innerText = productTotal;
 
+  theTotal+=productTotal;
  }
 
-
-
-
- 
-
-
- 
-
+  document.querySelector('h2 > span').innerText = theTotal;
 }
 
 
-document.querySelector('.btn-delete').onclick = function(e){
 
-  console.log(e.currentTarget.parentElement.parentElement);
- }
+
+
+
+let deleteBtns = document.getElementsByClassName('btn-delete');
+
+for(let i=0; i< deleteBtns.length; i++){
+    deleteBtns[i].onclick = deleteTheRow;
+}
+
+function deleteTheRow(event){
+  event.currentTarget.parentElement.parentElement.remove();
+}
+// ^this version is with a named function
+
+
+// for(let i=0; i< deleteBtns.length; i++){
+//   deleteBtns[i].onclick = function(){
+//     event.currentTarget.parentElement.parentElement.remove();
+//   }
+// }
+// ^ this version is with an anonymous function, its a tiny bit shorter
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.getElementById('create').onclick = function(){
+
+  let newName = document.querySelector('.new > td > input[type="text"]').value;
+
+  let newPrice = document.querySelector('.new > td > input[type="number"]').value;
+
+ 
+let newRowInnerHtml = `
+
+<td class="name">
+  <span>${newName}</span>
+</td>
+
+<td class="pu">
+  $<span>${newPrice}</span>
+</td>
+
+<td class="qty">
+  <label>
+    <input type="number" value="0" min="0">
+  </label>
+</td>
+
+<td class="subtot">
+  $<span>0</span>
+</td>
+
+<td class="rm">
+  <button class="btn btn-delete">Delete</button>
+</td>
+
+`
+
+let newRow = document.createElement('tr');
+newRow.classList.add('product');
+
+newRow.innerHTML = newRowInnerHtml;
+
+newRow.querySelector('.btn-delete').onclick = deleteTheRow;
+
+document.querySelector('tbody').appendChild(newRow);
+
+
+
+ document.querySelector('.new > td > input[type="text"]').value = "";
+
+document.querySelector('.new > td > input[type="number"]').value = "";
+
+}
+
 
 
 
